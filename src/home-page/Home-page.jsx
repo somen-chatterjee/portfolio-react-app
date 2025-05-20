498c5950b4bd7f3b9b4bb5337fb0a6e2c71df66e
somendreamcast <somen@dreamcast.co>
2025-05-20 18:17:38 +0530

    set headres styles

--- src/home-page/Home-page.jsx
+++ src/home-page/Home-page.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Headers from "./components/headers";
 
 function HomePage() {
--- src/home-page/components/headers.jsx
+++ src/home-page/components/headers.jsx
@@ -12,50 +12,35 @@ import React from "react";
 
 function Headers() {
     return (
-        <header className="flex flex-row items-center justify-between sm:justify-around p-2 border-b-2 bg-gray-100">
-            <a
-                href="/"
-                className="bg-[#A53DFF] w-12 h-12 rounded-full flex items-center justify-center"
-            >
-                <span className="text-white font-medium text-lg">S</span>
-            </a>
-            <nav className="hidden sm:flex items-center gap-x-4 font-semibold">
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        Home
-                    </span>
-                </a>
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        About
-                    </span>
-                </a>
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        Process
-                    </span>
-                </a>
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        Protfolio
-                    </span>
-                </a>
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        Blog
-                    </span>
-                </a>
-                <a href="#">
-                    <span className="text-[#333333] font-medium text-xs">
-                        Services
-                    </span>
-                </a>
-
-                <a href="#" className="bg-[#A53DFF] p-1.5">
-                    <span className="text-white font-semibold text-xs">
-                        Contact
-                    </span>
-                </a>
+        <header className="flex flex-row items-center justify-between sm:justify-around pt-4">
+            <div className="flex flex-row items-center">
+                <div className="bg-[#A53DFF] w-12 h-12 rounded-full flex items-center justify-center mr-3">
+                    <span className="text-white font-medium text-2xl">S</span>
+                </div>
+                <div className="text-black font-medium text-3xl">Somen</div>
+            </div>
+            <nav className="hidden sm:flex items-center gap-12 font-semibold">
+                <button className="text-[#333333] font-normal text-xs">
+                    Home
+                </button>
+                <button className="text-[#333333] font-normal text-xs">
+                    About
+                </button>
+                <button className="text-[#333333] font-normal text-xs">
+                    Process
+                </button>
+                <button className="text-[#333333] font-normal text-xs">
+                    Protfolio
+                </button>
+                <button className="text-[#333333] font-normal text-xs">
+                    Blog
+                </button>
+                <button className="text-[#333333] font-normal text-xs">
+                    Services
+                </button>
+                <button className="bg-[#A53DFF] px-6 py-3 rounded-lg text-white font-normal text-xs">
+                    Contact
+                </button>
             </nav>
         </header>
     );
